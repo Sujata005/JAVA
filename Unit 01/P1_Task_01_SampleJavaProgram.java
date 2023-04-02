3ddc6449b298e71774492d8b26b4182eb45418b8
Sujata <112793420+Sujata005@users.noreply.github.com>
2023-04-02 20:44:23 +0530

    01

---
+++ Unit 01/P1_Task_01_SampleJavaProgram.java
@@ -0,0 +1,13 @@
+package Unit01;
+
+public class P1_Task_01_SampleJavaProgram{
+	public static void main(String[] args) {
+		System.out.println("Heloo world!");
+	}
+}
+
+
+/*
+OUTPUT:
+Heloo world!
+*/

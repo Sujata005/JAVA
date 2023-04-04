740077fbe8fde0dbd4adeffe08352cd78f5f5216
Sujata <112793420+Sujata005@users.noreply.github.com>
2023-04-04 14:52:43 +0530

    02

--- Unit 01/P2_Task01_KeywordsInJava.java
+++ Unit 01/P2_Task01_KeywordsInJava.java
@@ -1,1 +1,22 @@
+package Unit01;
 
+public class P2_Task01_KeywordsInJava {
+	public static void main(String[] args) {
+		//	    int double=90;  //Error can't assign variable name as a keyword
+		//Syntax error on token "double", invalid VariableDeclaratorId
+		//		abstract final; 
+	}
+	//ERROR We can assign Function name as s keyword;
+	//Syntax error on token "int", this expected
+	//	static void int(){   
+	//		System.out.println("Inside function");
+	//	}
+	//	static class abstract  //Error class name is assigned as keyword
+	//	{
+	//		//Syntax error on token "abstract", Identifier expected
+	//		int a,b;
+	//	}
+	//	abstract final;    //error object name is assigned as keyword
+
+
+}

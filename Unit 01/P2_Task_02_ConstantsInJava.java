bf1753f57e7d8a400f2424b5ac5fcb6fdd0f9074
Sujata <112793420+Sujata005@users.noreply.github.com>
2023-04-07 11:23:52 +0530

    02

--- Unit 01/P2_Task_02_ConstantsInJava.java
+++ Unit 01/P2_Task_02_ConstantsInJava.java
@@ -1,6 +1,6 @@
 package Unit01;
 
-public class P2_Task02_ConstantsInJava {
+public class P2_Task_02_ConstantsInJava {
 	protected static final double PRICE=234.90;
     public static final double PRICE2=65.90;
 	public static void main(String[] args) {
